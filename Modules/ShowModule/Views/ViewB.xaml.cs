988dab63107eb2bbd7676d91f0deb33b584f3232
贾世唯 <jswjia@163.com>
2024-10-17 17:52:35 +0800

    init

--- Modules/ShowModule/Views/ViewB.xaml.cs
+++ Modules/ShowModule/Views/ViewB.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -23,6 +25,25 @@ namespace ShowModule.Views
         public ViewB()
         {
             InitializeComponent();
+            LoadModel("3dModel/bugatti.obj");
+        }
+
+
+        private void LoadModel(string filePath)
+        {
+            // 使用 HelixToolkit 加载模型
+            var importer = new ModelImporter();
+            try
+            {
+                // 导入模型
+                var model = importer.Load(filePath);
+                var modelVisual = new ModelVisual3D { Content = model };
+                viewport3D.Children.Add(modelVisual); // 将模型添加到视口中
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"加载模型失败: {ex.Message}");
+            }
         }
     }
 }
