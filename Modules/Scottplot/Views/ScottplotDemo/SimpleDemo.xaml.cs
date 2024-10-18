c15ad921877969492dc5b46bf5344124c93eb699
贾世唯 <jswjia@163.com>
2024-10-18 18:03:33 +0800

    init

--- Modules/Scottplot/Views/ScottplotDemo/SimpleDemo.xaml.cs
+++ Modules/Scottplot/Views/ScottplotDemo/SimpleDemo.xaml.cs
@@ -24,51 +24,16 @@ namespace Scottplot.Views.ScottplotDemo
     {
 
         
-        private readonly Random rand;
-        private double[] xs, ys;
-        private int count = 0;
         public SimpleDemo()
         {
             InitializeComponent();
 
-            
-                rand = new Random();
-                xs = new double[100];
-                ys = new double[100];
-            ScottPlot.Plot myPlot = new();
-            myPlot = WpfPlot1.Plot.Add.Scatter(xs, ys);
-                FormsPlot1.Refresh();
 
-                // 设置定时器每隔一段时间更新数据
-                System.Timers.Timer timer = new System.Timers.Timer(100); // 100毫秒更新一次
-                timer.Elapsed += TimerElapsed;
-                timer.Start();
-            
+            double[] dataX = { 1, 2, 3, 4, 5 };
+            double[] dataY = { 1, 4, 9, 16, 25 };
+            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+            WpfPlot1.Refresh();
         }
 
-        private void TimerElapsed(object sender, ElapsedEventArgs e)
-        {
-            // 更新数据
-            if (count < 100)
-            {
-                xs[count] = count; // X 轴值
-                ys[count] = rand.NextDouble(); // Y 轴随机值
-                count++;
-            }
-            else
-            {
-                // 移动窗口
-                for (int i = 0; i < 99; i++)
-                {
-                    xs[i] = xs[i + 1];
-                    ys[i] = ys[i + 1];
-                }
-                xs[99] = count; // 添加新的X值
-                ys[99] = rand.NextDouble(); // 添加新的Y值
-            }
-           
-            // 刷新图表
-            WpfPlot1.Invoke(new Action(() => FormsPlot1.Refresh()));
-        }
     }
 }
