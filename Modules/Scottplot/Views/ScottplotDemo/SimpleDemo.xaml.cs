e91c003a00c8358017b50816fe53e00a791efb4a
贾世唯 <jswjia@163.com>
2024-10-21 18:00:22 +0800

    init

--- Modules/Scottplot/Views/ScottplotDemo/SimpleDemo.xaml.cs
+++ Modules/Scottplot/Views/ScottplotDemo/SimpleDemo.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Timers;
 using System.Windows;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Scottplot.Views.ScottplotDemo
 {
@@ -23,17 +25,51 @@ namespace Scottplot.Views.ScottplotDemo
     public partial class SimpleDemo : UserControl
     {
 
-        
+        private readonly Random _random = new Random();
+        private readonly double[] _dataX;
+        private readonly double[] _dataY;
+        private int _pointCount = 100;
         public SimpleDemo()
         {
             InitializeComponent();
+            _dataX = new double[_pointCount];
+            _dataY = new double[_pointCount];
+            this.Loaded+=SimpleDemo_Loaded;
+            
 
-
-            double[] dataX = { 1, 2, 3, 4, 5 };
-            double[] dataY = { 1, 4, 9, 16, 25 };
-            WpfPlot1.Plot.Add.Scatter(dataX, dataY);
-            WpfPlot1.Refresh();
         }
 
+        private void SimpleDemo_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+
+            // 初始化 X 数据
+            for (int i = 0; i < _pointCount; i++)
+            {
+                _dataX[i] = i;
+            }
+
+            // 设置定时器
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1); // 每秒触发一次
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            
+            // 实时更新 Y 数据
+            for (int i = 0; i < _pointCount - 1; i++)
+            {
+                _dataY[i] = _dataY[i + 1]; // 把数据往前移动
+            }
+            _dataY[_pointCount - 1] = _random.NextDouble(); // 生成新的随机数据
+
+            // 绘制图表
+            WpfPlot1.Plot.Clear(); // 清除之前的图形
+            WpfPlot1.Plot.Add.Scatter(_dataX, _dataY); // 添加新的数据
+            WpfPlot1.Refresh(); // 刷新图表
+        }
     }
 }
--- Modules/ShowModule/Views/ViewA.xaml.cs
+++ Modules/ShowModule/Views/ViewA.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -15,6 +16,21 @@ using System.Windows.Shapes;
 
 namespace ShowModule.Views
 {
+    public class VisualHost : UIElement
+    {
+        public Visual Visual { get; set; }
+
+        protected override int VisualChildrenCount
+        {
+            get { return Visual != null ? 1 : 0; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return Visual;
+        }
+    }
+
     /// <summary>
     /// Interaction logic for ViewA.xaml
     /// </summary>
@@ -22,7 +38,61 @@ namespace ShowModule.Views
     {
         public ViewA()
         {
+            
             InitializeComponent();
+            _drawingVisual = new DrawingVisual();
+            _dataPoints = new List<double>();
+            _random = new Random();
+            DrawInitialLines();
+            MyCanvas.Children.Add(new VisualHost() { Visual = _drawingVisual });
+        }
+        private DrawingVisual _drawingVisual;
+        private List<double> _dataPoints;
+        private Random _random;
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Timer timer = new Timer();
+            timer.Interval=1000;
+            timer.Elapsed += (s, e) =>
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    double newDataPoint = _random.Next(1, 50);
+                    _dataPoints.Add(newDataPoint);
+                    DrawLines();
+                });
+
+            };
+            timer.Start();
+        }
+
+
+        private void DrawInitialLines()
+        {
+            using (DrawingContext drawingContext = _drawingVisual.RenderOpen())
+            {
+                // 绘制坐标系
+                Pen axisPen = new Pen(Brushes.Black, 1);
+                drawingContext.DrawLine(axisPen, new Point(50, 250), new Point(550, 250)); // X 轴
+                drawingContext.DrawLine(axisPen, new Point(50, 250), new Point(50, 50)); // Y 轴
+            }
+        }
+
+        private void DrawLines()
+        {
+            using (DrawingContext drawingContext = _drawingVisual.RenderOpen())
+            {
+                Pen linePen = new Pen(Brushes.Blue, 2);
+
+                // 绘制所有的折线段
+                for (int i = 0; i < _dataPoints.Count - 1; i++)
+                {
+                    Point startPoint = new Point(50 + i * 70, 250 - _dataPoints[i] * 5); // X轴位置和Y轴高度
+                    Point endPoint = new Point(50 + (i + 1) * 70, 250 - _dataPoints[i + 1] * 5);
+                    drawingContext.DrawLine(linePen, startPoint, endPoint);
+                }
+            }
         }
     }
 }
